58fc3475714360ca59cd65ef5699bc0f395b0179
Jaimin <jaiminpandya@example.com>
2014-07-27 04:26:42 +0530

    Add some code in stylesheet

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "user attributes must not be empty" do 
+   user = User.new
+   assert user.invalid?
+   assert user.errors[:name].any?
+   assert user.errors[:email].any?
+   assert user.errors[:password].any?
+   assert user.errors[:password_confirmation].any?
+  end
 end
